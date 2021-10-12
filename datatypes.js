8614cd2790fdf7e5d502235a6955bd449f75ca8a
bennu <benarzee1212@gmail.com>
2021-10-12 08:06:32 +0530

    new

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+var sum=10+10+5
+console.log(sum);
+
+
+var num1=21
+var num2=10
+var diff=num1-num2
+console.log(diff);
+
+var num1=21
+var num2=10
+var mul=num1*num2
+console.log(mul);
+
+var num1=21
+var num2=10
+var div=num1/num2
+console.log(div);
+
+var num1=21
+var num2=10
+var divByZero=num1/0
+console.log(divByZero);  
+
+
+
+var num1=21
+var mulByString=num1*"hello"
+console.log(mulByString);
+console.log(typeof(mulByString));
+// output is not a number but still data type is number if we check console.log(typeof(mulByString))
